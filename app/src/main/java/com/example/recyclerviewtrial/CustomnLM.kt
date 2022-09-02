f28a6b71e8ffc7c26ef093ef1290784132b930de
poojappd <poojapriyadarshnips@gmail.com>
2022-09-02 11:28:53 +0530

    for backup 2

---
+++ app/src/main/java/com/example/recyclerviewtrial/CustomnLM.kt
@@ -0,0 +1,14 @@
+package com.example.recyclerviewtrial
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomnLM :RecyclerView.LayoutManager() {
+    override fun generateDefaultLayoutParams(): RecyclerView.LayoutParams {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemViewType(view: View): Int {
+        return super.getItemViewType(view)
+    }
+}
